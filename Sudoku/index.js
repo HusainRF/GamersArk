e2e9e5e42280654913961c040da21e7234ac5c72
HusainRF <justgetupanddoit9@gmail.com>
2022-09-11 16:45:55 +0530

    edit input cell in table

--- Sudoku/index.js
+++ Sudoku/index.js
@@ -1,3 +1,4 @@
+
 // creating a matrix 
 let table = document.createElement('table');
 let tbody = document.createElement('tbody');
@@ -7,25 +8,45 @@ table.appendChild(tbody);
 // Adding the entire table into the matrix class
 document.querySelector('.matrix').appendChild(table);
 
+
+function checkInput(ob) {
+    //to check input should be 1-9
+    var invalidChars = /[^0-9]/gi
+    if (invalidChars.test(ob.value)) {
+        ob.value = ob.value.replace(invalidChars, "");
+    }
+};
+
 function create_table() {
     // function declation (this is the main part which creates the table of size(9*9) )
 
     for (i = 1; i <= 9; i++) {
-        
+
         var row = document.createElement("tr");
-        
+
         for (j = 1; j <= 9; j++) {
             cell = document.createElement("td");
-            cell.innerHTML = j ;
+            // inp = it stores the user input when the feild is empty
+            let inp = '<input class="inp" maxlength="1" onChange="checkInput(this)" onKeyup="checkInput(this)" type="text" autocomplete="off"/>';
 
-            if(i%3==0 && j%3 == 0 )
-            cell.setAttribute("style","border-right:3px solid black ;border-bottom:3px solid black;" );
-            else if(i%3==0  && j%3 !=0 )
-            cell.setAttribute("style","border-bottom:3px solid black; border-right:3px solid #D6CDA4;" );
-            else if(j%3==0 && i%3 !=0)
-            cell.setAttribute("style","border-right:3px solid black ; border-bottom  :3px solid #D6CDA4 ;" );
-            else 
-               cell.setAttribute("style","border-right:3px solid #D6CDA4 ; border-bottom  :3px solid #D6CDA4 ;" );
+            if (i == j) {
+                // input for empty feilds...
+                cell.innerHTML = inp;
+                console.log(inp);
+            }
+            else {
+                // fixed value for rest of cell....
+                cell.innerHTML = 1;
+            }
+
+            if (i % 3 == 0 && j % 3 == 0)
+                cell.setAttribute("style", "border-right:3px solid black ;border-bottom:3px solid black;");
+            else if (i % 3 == 0 && j % 3 != 0)
+                cell.setAttribute("style", "border-bottom:3px solid black; border-right:3px solid #D6CDA4;");
+            else if (j % 3 == 0 && i % 3 != 0)
+                cell.setAttribute("style", "border-right:3px solid black ; border-bottom  :3px solid #D6CDA4 ;");
+            else
+                cell.setAttribute("style", "border-right:3px solid #D6CDA4 ; border-bottom  :3px solid #D6CDA4 ;");
             row.append(cell);
 
 
