f4639025f181fb1014b9c233633081dfefa20c3a
Chirag Jain <chiragjain0406@gmail.com>
2022-09-12 16:58:05 +0530

    solution matrix created

--- Sudoku/index.js
+++ Sudoku/index.js
@@ -1,3 +1,83 @@
+// initailzation of matrix ended xx---xx
+var trail = new Array(9);
+for (var i = 0; i < trail.length; i++) {
+    trail[i] = new Array(9);
+
+}
+
+for(let i= 0;i < 9 ; i ++){
+    for(let j = 0 ;j <9 ;j++){
+        trail[i][j] = 1;
+    }
+    console.log(trail[i]);
+}
+
+
+var checker = new Array(9);
+for (var i = 0; i < checker.length; i++) {
+    checker[i] = new Array(9);
+
+}
+
+for(let i= 0;i < 9 ; i ++){
+    for(let j = 0 ;j <9 ;j++){
+        if(i === j)
+          checker[i][j] = "*";
+         else 
+           checker[i][j] = trail[i][j];
+        }
+        console.log(checker[i]);
+    }
+    
+    var user = new Array(9);
+    for (var i = 0; i < user.length; i++) {
+    user[i] = new Array(9);
+
+}
+
+for(let i= 0;i < 9 ; i ++){
+    for(let j = 0 ;j <9 ;j++){
+        if(i == j)
+          user[i][j] = '*';
+        else 
+          user[i][j] = trail[i][j];
+    }
+    console.log(user[i]);
+}
+// initailzation of matrix ended xx---xx
+
+
+// genrating rand no. for dipalying
+let level = 0;
+// when level = 0 dificulty => easy
+// when level = 1 dificulty => medium
+// when level = 2 dificulty => hard
+let cnt_display = 30;
+function hard1(event) {
+    level = 2;
+    cnt_display = 30 + Math.floor((Math.random() * 10)); // genrate rand value 30-40 
+    console.log(cnt_display);
+}
+
+function easy1() {
+    level = 0;
+    cnt_display = 50 + Math.floor((Math.random() * 10)); // genrate rand value 50-60
+    console.log(cnt_display);
+}
+function medium1() {
+    level = 1;
+    cnt_display = 40 + Math.floor((Math.random() * 10)); // genrate rand value 40-50
+    console.log(cnt_display);
+}
+
+document.getElementById('easy').addEventListener('click', easy1);
+document.getElementById('medium').addEventListener('click', medium1);
+let var_hard = document.getElementById('hard').addEventListener('click', hard1);
+// var_hard.addEventListener("click"
+
+
+// genrating rand no. for dipalying
+
 
 // creating a matrix 
 let table = document.createElement('table');
@@ -6,12 +86,12 @@ table.appendChild(tbody);
 // table.setAttribute("style","border:1px solid #D6CDA4" )
 
 // Adding the entire table into the matrix class
-document.querySelector('.matrix').appendChild(table);
 
+document.querySelector('.matrix').appendChild(table);
 
 function checkInput(ob) {
     //to check input should be 1-9
-    var invalidChars = /[^0-9]/gi
+    var invalidChars = /[^1-9]/gi
     if (invalidChars.test(ob.value)) {
         ob.value = ob.value.replace(invalidChars, "");
     }
@@ -29,14 +109,13 @@ function create_table() {
             // inp = it stores the user input when the feild is empty
             let inp = '<input class="inp" maxlength="1" onChange="checkInput(this)" onKeyup="checkInput(this)" type="text" autocomplete="off"/>';
 
-            if (i == j) {
+            if (checker[i-1][j-1]==='*') {
                 // input for empty feilds...
                 cell.innerHTML = inp;
-                console.log(inp);
             }
             else {
                 // fixed value for rest of cell....
-                cell.innerHTML = 1;
+                cell.innerHTML = trail[i-1][j-1];
             }
 
             if (i % 3 == 0 && j % 3 == 0)
